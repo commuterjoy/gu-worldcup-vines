fbfb17ac4adbd3e8c3ba20891c3cb2d07ee6caa6
Matt Chadburn <matt.chadburn@ft.com>
2014-06-28 20:19:38 +0100

    Trim team string

--- gooooooooooooooal.js
+++ gooooooooooooooal.js
@@ -2,7 +2,7 @@
     var sendRequest = function (teams) {
         console.log("gu: Sending request for ", teams);
         var req = new XMLHttpRequest();
-        req.open("GET", "https://twitter.com/search?q="+teams+"vine.co", true);
+        req.open("GET", "https://twitter.com/search?q="+teams+"%20vine.co", true);
         req.onreadystatechange = function() {
             if (req.readyState == 4) {
                 if (req.status == 200) {
@@ -39,7 +39,7 @@
             var teams = document.querySelectorAll('.team__name');
             var scores = document.querySelectorAll('.team__score');
             console.log('gu:', teams, scores);
-            sendRequest(teams[0].textContent + '%20' + teams[1].textContent);
+            sendRequest(teams[0].textContent.trim() + '%20' + teams[1].textContent.trim());
         }
 
         var css = document.createElement('style');
@@ -48,7 +48,7 @@
         document.body.appendChild(css);
 
         window.setInterval(fetch, 30000);
-        fetch();
+        window.setTimeout(fetch, 5000);
     })
 
 }).call(this);
